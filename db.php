0b1869e0e96670f680d15cc56ac37cc733f233ed
Abdelilah <abdouajjouqa@gmail.com>
2025-01-06 17:31:12 +0100

    Refactor database connection to use PDO with error handling

--- db.php
+++ db.php
@@ -4,8 +4,22 @@ $user = 'root';
 $password = '123';
 $database = 'blog_2';
 
-$conn = new mysqli($host, $user, $password, $database);
+// $conn = new mysqli($host, $user, $password, $database);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+// try {
+//     $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+// } catch (PDOException $e){
+//     echo 'Connection failed: ' . $e->getMessage();
+// }
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
 }
