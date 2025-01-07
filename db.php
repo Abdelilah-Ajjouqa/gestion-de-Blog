97e69288643c6abc22df3af1ed35992f3a0e20cc
Abdelilah <abdouajjouqa@gmail.com>
2025-01-07 12:02:51 +0100

    Refactor database connection and remove unused article page

--- db.php
+++ db.php
@@ -1,25 +1,9 @@
 <?php
-$host = 'localhost';
 $user = 'root';
 $password = '123';
-$database = 'blog_2';
-
-// $conn = new mysqli($host, $user, $password, $database);
-
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
-// try {
-//     $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
-// } catch (PDOException $e){
-//     echo 'Connection failed: ' . $e->getMessage();
-// }
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
-    // Set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
+    $conn = new PDO("mysql:host=localhost;dbname=blog_2", $user, $password);
+} catch (PDOException $e){
     echo 'Connection failed: ' . $e->getMessage();
 }
